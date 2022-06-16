1890e5c7c8cc4802f7be02bf2976cdccaab7c86b
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-06-16 14:06:12 -0400

    created an array to store todos as well as a function to add them to that array

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <script setup lang="ts">
+import { reactive, ref } from 'vue';
 
+const todoInput = ref("")
+const todos = reactive([])
+function addTodo() {
+  const todo = {
+    text: todoInput.value,
+    done: false
+  }
+  todos.push(todo)
+  todoInput.value = ""
+}
 </script>
 
 <template>
-  <h1>Just Another Todo</h1>
-  <form>
-    <input type="text">
+  <pre>{{ todos }}</pre>
+  <h1>Just Another Todo List</h1>
+  <form @submit.prevent="addTodo()">
+    <input v-model="todoInput" type="text">
     <input type="submit" value="Add">
   </form>
+  <ul>
+    <li v-for="todo in todos">
+      {{ todo.text }}
+    </li>
+  </ul>
 </template>
 
 <style>
