29e32cc1d80d805e8d9f739151a0c75e213181c7
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-06-16 15:37:36 -0400

    add line through todos when they have a completed status

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,30 @@ import { ref } from 'vue';
 import type { Ref } from 'vue';
 import { useTodoList } from '@/stores/todoList'
 
-const storedList = useTodoList()
+const todoStore = useTodoList()
 
 // Form Data
 const todoInput: Ref<string> = ref("")
 
 // List Methods
 const addTodo = (): void => {
-  storedList.addTodo(todoInput.value)
+  todoStore.addTodo(todoInput.value)
   todoInput.value = ""
 }
 
 </script>
 
 <template>
-  <pre>{{ storedList }}</pre>
   <h1>Just Another Todo List</h1>
   <form @submit.prevent="addTodo()">
     <input v-model="todoInput" type="text">
     <input type="submit" value="Add">
   </form>
   <ul>
-    <li v-for="todoItem in storedList.items">
-      {{ todoItem.text }}
-      <button @click="storedList.completeTodo(todoItem.id)">Complete</button>
-      <button @click="storedList.deleteTodo(todoItem.id)">Delete</button>
+    <li v-for="todoItem in todoStore.items">
+      <span :style="todoItem.done ? 'text-decoration: line-through;' : ''">{{ todoItem.text }}</span>
+      <button @click="todoStore.completeTodo(todoItem.id)">Complete</button>
+      <button @click="todoStore.deleteTodo(todoItem.id)">Delete</button>
     </li>
   </ul>
 </template>
