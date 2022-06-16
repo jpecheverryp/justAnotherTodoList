349ddb01c07688735be51d4e427ed9c7e1046f26
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-06-16 14:24:19 -0400

    commented sections on app and replaced reactive for ref

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue';
+import { ref } from 'vue';
 
+// Form Data
 const todoInput = ref("")
-const todos = reactive([])
-function addTodo() {
+
+// List Data
+const todos = ref([])
+
+// List Methods
+const addTodo = () => {
   const todo = {
     text: todoInput.value,
     done: false
   }
-  todos.push(todo)
+  todos.value.push(todo)
   todoInput.value = ""
 }
 </script>
@@ -16,13 +21,13 @@ function addTodo() {
 <template>
   <pre>{{ todos }}</pre>
   <h1>Just Another Todo List</h1>
-  <form @submit.prevent="addTodo()">
+  <form @submit.prevent="addTodo">
     <input v-model="todoInput" type="text">
     <input type="submit" value="Add">
   </form>
   <ul>
-    <li v-for="todo in todos">
-      {{ todo.text }}
+    <li v-for="todoItem in todos">
+      {{ todoItem.text }}
     </li>
   </ul>
 </template>
