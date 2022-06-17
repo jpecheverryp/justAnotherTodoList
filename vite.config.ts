ebde7ec684be828f4777a15ebd2d6e77b6149bc3
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-06-17 11:48:32 -0400

    configure unocss library

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+import { presetAttributify, presetUno } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Unocss({
+      presets: [
+        presetAttributify({}),
+        presetUno()
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
